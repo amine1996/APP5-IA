793098c753c59a8155ddd79488dd31771d57a61a
Thierry Girard <girard.thierry80@gmail.com>
2017-09-20 13:38:54 +0200

    création TP02

---
+++ TP02/src/main/java/monjeu/dominos/HeuristiquesDominos.java
@@ -0,0 +1,30 @@
+package monjeu.dominos;
+
+import jeux.alg.Heuristique;
+import jeux.modele.PlateauJeu;
+import jeux.modele.joueur.Joueur;
+
+
+public class HeuristiquesDominos{
+
+	public static  Heuristique hblanc = new Heuristique(){
+				
+		public int eval(PlateauJeu p, Joueur j){
+			PlateauDominos pDomino = (PlateauDominos) p;
+			return 1000/(1 + pDomino.nbCoupsBlanc());
+		}
+	};
+
+	public static  Heuristique hnoir = new Heuristique(){
+	
+		public int eval(PlateauJeu p, Joueur j){
+			PlateauDominos pDomino = (PlateauDominos) p;
+
+			if(pDomino.nbCoupsBlanc() == 0)
+				return Integer.MIN_VALUE;
+
+			return -(1000 * pDomino.nbCoupsNoir());
+		}
+	};
+
+}
